201d6c7701cab475b4905f5294c5d91f3e65e7d7
Anjali-1206 <anjaligirnale12@gmail.com>
2024-11-06 23:27:17 +0530

    Update footer.php

--- include/footer.php
+++ include/footer.php
@@ -6,14 +6,14 @@
 						<hr class="white-bar">
 
 							<h1 class="text-center">
-								<a  target="_blank" href="https://www.facebook.com/exceptionalprogrammers"><i style="color: #fff;" class="fab fa-facebook-square"></i></a>
+								<a  target="_blank" href="https://gcoea.ac.in/"><i style="color: #fff;" class="fab fa-facebook-square"></i></a>
 
-								<a target="_blank" href="https://www.youtube.com/exceptionalprogrammers"><i style="color: #fff;" class="fab fa-youtube-square"></i></a> 
+								<a target="_blank" href="https://gcoea.ac.in/"><i style="color: #fff;" class="fab fa-youtube-square"></i></a> 
 							</h1>
 
 							<div class="details">
-								 <p> 92 300 1234567 <i class="fa fa-phone"></i>
-								exceptionalprogrammers@gmail.com <i class="fa fa-envelope"></i>  </p>
+								 <p>  9800678907 <i class="fa fa-phone"></i>
+								gcoea@gmail.com <i class="fa fa-envelope"></i>  </p>
 							</div>
 
 					</div>
